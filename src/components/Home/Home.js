47240351916c7b79501de7c7b492f98e5c213d94
rajon111 <mostofarajon111@gmail.com>
2023-02-06 08:11:53 +0600

    react practice

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const Home = () => {
+  return (
+    <div>
+        <Link to='/home'>Home</Link>
+        Home
+    </div>
+  )
+}
